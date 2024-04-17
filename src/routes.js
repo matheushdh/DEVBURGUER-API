3140dcd6e24147a20d5e8c3798eb8654c1299338
matheushdh <matmou.2017@gmail.com>
2024-04-17 08:28:59 -0300

    ex

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import UserController from './controllers/Usercontroller';
+import SessionControler from './controllers/SessionControler';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
+routes.post('/session', SessionControler.store);
 
 export default routes;
